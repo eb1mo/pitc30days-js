fe9cdca649d048159a3f4605918256b1c335d39a
Bimochan Poudel <bimochanpoudel2060@gmail.com>
2024-05-15 22:03:59 +0545

    datatype conversions

---
+++ Day2/03_conversionOperation.js
@@ -0,0 +1,41 @@
+// let score = 123
+// let scoreString = "123"
+// let scoreString = "123abc"
+// let score1 = null
+// let score1 = undefined
+// let score1 = true
+// let score1 = "bimo"
+
+// console.log(typeof(score));
+// console.log(typeof(score1));
+
+// let valueInNumber = Number(score1); //converts to number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "123" => 123
+// "123abc" => NaN
+// true => 1
+// false => 0
+
+// let isLoggedIn = 1;
+// let isLoggedIn = 123;
+// let isLoggedIn = "bimo";
+// let isLoggedIn = "";
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "bimo" => true
+// 123 => true
+
+
+let anyNumber = 123;
+let stringNumber = String(anyNumber);
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
